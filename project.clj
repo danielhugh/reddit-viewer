753c9913b73405254cd100468295165b9b206645
Daniel Hugh <daniel.hugh@mail.utoronto.ca>
2018-12-22 22:59:45 -0500

    Add re-frame-10x dependency for debugging.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
                  [org.clojure/clojurescript "1.10.439" :scope "provided"]
@@ -15,38 +15,40 @@
             [lein-figwheel "0.5.16"]]
 
   :clean-targets ^{:protect false}
-  [:target-path
-   [:cljsbuild :builds :app :compiler :output-dir]
-   [:cljsbuild :builds :app :compiler :output-to]]
+[:target-path
+ [:cljsbuild :builds :app :compiler :output-dir]
+ [:cljsbuild :builds :app :compiler :output-to]]
 
   :resource-paths ["public"]
 
   :figwheel {:http-server-root "."
-             :nrepl-port 7002
+             :nrepl-port       7002
              :nrepl-middleware [cider.piggieback/wrap-cljs-repl]
-             :css-dirs ["public/css"]}
+             :css-dirs         ["public/css"]}
 
   :cljsbuild {:builds {:app
                        {:source-paths ["src" "env/dev/cljs"]
                         :compiler
-                        {:main "reddit-viewer.dev"
-                         :output-to "public/js/app.js"
-                         :output-dir "public/js/out"
-                         :asset-path   "js/out"
-                         :source-map true
-                         :optimizations :none
-                         :pretty-print  true}
+                                      {:closure-defines {"re_frame.trace.trace_enabled_QMARK_" true}
+                                       :preloads        [day8.re-frame-10x.preload]
+                                       :main            "reddit-viewer.dev"
+                                       :output-to       "public/js/app.js"
+                                       :output-dir      "public/js/out"
+                                       :asset-path      "js/out"
+                                       :source-map      true
+                                       :optimizations   :none
+                                       :pretty-print    true}
                         :figwheel
-                        {:on-jsload "reddit-viewer.core/mount-root"
-                         :open-urls ["http://localhost:3449/index.html"]}}
+                                      {:on-jsload "reddit-viewer.core/mount-root"
+                                       :open-urls ["http://localhost:3449/index.html"]}}
                        :release
                        {:source-paths ["src" "env/prod/cljs"]
                         :compiler
-                        {:output-to "public/js/app.js"
-                         :output-dir "public/js/release"
-                         :asset-path   "js/out"
-                         :optimizations :advanced
-                         :pretty-print false}}}}
+                                      {:output-to     "public/js/app.js"
+                                       :output-dir    "public/js/release"
+                                       :asset-path    "js/out"
+                                       :optimizations :advanced
+                                       :pretty-print  false}}}}
 
   :aliases {"package" ["do" "clean" ["cljsbuild" "once" "release"]]}
 
@@ -54,4 +56,5 @@
                    :dependencies [[binaryage/devtools "0.9.10"]
                                   [figwheel-sidecar "0.5.16"]
                                   [nrepl "0.4.4"]
-                                  [cider/piggieback "0.3.8"]]}})
+                                  [cider/piggieback "0.3.8"]
+                                  [day8.re-frame/re-frame-10x "0.3.6-react16"]]}})
