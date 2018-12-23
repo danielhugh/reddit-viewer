f81257cc18b49f7df1096c50cd26af0ca366caf7
hughdani <daniel.hugh@mail.utoronto.ca>
2018-12-22 22:40:10 -0500

    Refactor to use re-frame.

--- src/reddit_viewer/chart.cljs
+++ src/reddit_viewer/chart.cljs
@@ -1,18 +1,19 @@
 (ns reddit-viewer.chart
   (:require
     [cljsjs.chartjs]
-    [reagent.core :as r]))
+    [reagent.core :as r]
+    [re-frame.core :as rf]))
 
 (defn render-data [node data]
   (js/Chart.
     node
     (clj->js
-      {:type "bar"
-       :data {:labels (map :title data)
-              :datasets [{:label "votes"
-                          :data (map :score data)}
-                         #_{:label "comments"
-                          :data (map :num_comments data)}]}
+      {:type    "bar"
+       :data    {:labels   (map :title data)
+                 :datasets [{:label "votes"
+                             :data  (map :score data)}
+                            #_{:label "comments"
+                               :data  (map :num_comments data)}]}
        :options {:scales {:xAxes [{:display false}]}}})))
 
 (defn destroy-chart [chart]
@@ -20,17 +21,19 @@
     (.destroy @chart)
     (reset! chart nil)))
 
-(defn render-chart [chart data]
+(defn render-chart [chart]
   (fn [component]
-    (when (not-empty @data)
-      (let [node (r/dom-node component)]
-        (destroy-chart chart)
-        (reset! chart (render-data node @data))))))
+    (when-let [posts @(rf/subscribe [:posts])]
+      (destroy-chart chart)
+      (reset! chart (render-data (r/dom-node component) posts)))))
 
-(defn chart-posts-by-votes [data]
-  (let [chart (r/atom nil)]
+(defn render-canvas []
+  (when @(rf/subscribe [:posts]) [:canvas]))
+
+(defn chart-posts-by-votes []
+  (let [chart (atom nil)]
     (r/create-class
-      {:component-did-mount (render-chart chart data)
-       :component-did-update (render-chart chart data)
+      {:component-did-mount    (render-chart chart)
+       :component-did-update   (render-chart chart)
        :component-will-unmount (fn [_] (destroy-chart chart))
-       :render (fn [] (when @data [:canvas]))})))
+       :render                 render-canvas})))
